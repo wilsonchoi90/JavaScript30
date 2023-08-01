40b831f40ad1afb817c5524c019f5890fdb12443
Wilson Choi <wilson.choi90@gmail.com>
2023-07-31 23:33:56 -0400

    Day 19 challenge completed

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -28,7 +28,10 @@ function paintToCanvas() {
         // take the pixels out
         let pixels = ctx.getImageData(0, 0, width, height);
         // mess with them
-        pixels = redEffect(pixels);
+        // pixels = redEffect(pixels);
+
+        pixels = rbgSplit(pixels);
+        ctx.globalAlpha = 0.1;
         // put them back
         ctx.putImageData(pixels, 0, 0);
     }, 16);
@@ -54,6 +57,42 @@ function redEffect(pixels) {
         pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
         pixels.data[i + 2] = pixels.data[i + 2] * 0.5 // BLUE
     }
+    return pixels;
+}
+
+function rbgSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0] + 100; // RED
+        pixels.data[i + 150] = pixels.data[i + 1] - 50; // GREEN
+        pixels.data[i - 550] = pixels.data[i + 2] * 0.5 // BLUE
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+
     return pixels;
 }
 
